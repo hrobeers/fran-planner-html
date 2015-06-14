5c6985966b868d2bef7d73de90c01f77a281451b
hrobeers <hansrobeers@gmail.com>
2015-06-14 11:06:55 +0200

    Table generation

---
+++ js/tables.js
@@ -0,0 +1,32 @@
+var moments = JSON.parse('{"moments":[{"starttime":"16:00:00","max_count":7,"location":"Gasthuis","id":1,"endtime":"17:00:00","date":"2015-05-05"},{"starttime":"18:00:00","max_count":5,"location":"Gasthuis","id":2,"endtime":"20:00:00","date":"2015-05-05"},{"starttime":"16:00:00","max_count":6,"location":"Thuis","id":2,"endtime":"17:00:00","date":"2015-05-07"}]}');
+
+function render_tables(table_div)
+{
+    $(table_div).html("<center><img id='loader' src='img/loader.gif' /></center>");
+
+    $.get('/mst/tables.mustache', function(template) {
+	var rendered = Mustache.render(template, moments);
+	$(table_div).html(rendered);
+    });
+}
+
+function transpose_table(table_id){
+    $(table_id).each(function() {
+        var $this = $(this);
+        var newrows = [];
+        $this.find("tr").each(function(){
+            var i = 0;
+            $(this).find("td").each(function(){
+                i++;
+                if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
+                newrows[i].append($(this));
+            });
+        });
+        $this.find("tr").remove();
+        $.each(newrows, function(){
+            $this.append(this);
+        });
+    });
+
+    return false;
+}
