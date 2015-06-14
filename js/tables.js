009030375769e23b9e1f2b7310d4f10b7f2ad2a8
hrobeers <hansrobeers@gmail.com>
2015-06-14 14:34:44 +0200

    Summary & navigation

--- js/tables.js
+++ js/tables.js
@@ -1,13 +1,35 @@
-var moments = JSON.parse('{"moments":[{"starttime":"16:00:00","max_count":7,"location":"Gasthuis","id":1,"endtime":"17:00:00","date":"2015-05-05"},{"starttime":"18:00:00","max_count":5,"location":"Gasthuis","id":2,"endtime":"20:00:00","date":"2015-05-05"},{"starttime":"16:00:00","max_count":6,"location":"Thuis","id":2,"endtime":"17:00:00","date":"2015-05-07"}]}');
+var moments = JSON.parse('{"moments":[{"starttime":"16:00:00","max_count":7,"location":"Gasthuis","id":1,"endtime":"17:00:00","date":"2015-05-05"},{"starttime":"18:00:00","max_count":5,"location":"Gasthuis","id":2,"endtime":"20:00:00","date":"2015-05-05"},{"starttime":"16:00:00","max_count":6,"location":"Thuis","id":3,"endtime":"17:00:00","date":"2015-05-07"}]}');
 
 function render_tables(table_div)
 {
-    $(table_div).html("<center><img id='loader' src='img/loader.gif' /></center>");
+    loader(table_div,
+	   function()
+	   {
+	       $.get('/mst/tables.mustache', function(template) {
+		   var rendered = Mustache.render(template, moments);
+		   $(table_div).html(rendered);
+	       });
+	   }
+	  );
+}
 
-    $.get('/mst/tables.mustache', function(template) {
-	var rendered = Mustache.render(template, moments);
-	$(table_div).html(rendered);
-    });
+function render_summary(table_div)
+{
+    loader(table_div,
+	   function()
+	   {
+	       $.get('/mst/summary.mustache', function(template) {
+		   var rendered = Mustache.render(template, moments);
+		   $(table_div).html(rendered);
+	       });
+	   }
+	  );
+}
+
+function loader(div, render_func)
+{
+    $(div).html("<center><img id='loader' src='img/loader.gif' /></center>");
+    render_func();
 }
 
 function transpose_table(table_id){
